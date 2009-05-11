4629f08e7002cbc7cd8ceece3d9e62126e1edf8e
Eddie McCreary <lazarus@heorot.org>
2009-05-11 11:15:09 -0500

    inital checking of clean plugin from wizard

---
+++ bookit2/defaults/preferences/bookit2.js
@@ -0,0 +1,5 @@
+pref("extensions.bookit2.boolpref", false);
+pref("extensions.bookit2.intpref", 0);
+pref("extensions.bookit2.stringpref", "A string");
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.bookit2@heorot.org.description", "chrome://bookit2/locale/bookit2.properties");
