165dce3258bd1353e077f44e2168d9bd30e94cf5
Eddie McCreary <lazarus@heorot.org>
2009-06-11 13:45:23 -0500

    capture stdout in runhidden for logging

--- runhidden/runhidden.cpp
+++ runhidden/runhidden.cpp
@@ -4,6 +4,9 @@
 #include "stdafx.h"
 #include "runhidden.h"
 
+// log file
+FILE *fl_log = NULL;
+void ReadFromPipe(HANDLE hRead) ;
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
                      HINSTANCE hPrevInstance,
@@ -26,9 +29,17 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 	}
 
 	LPWSTR lpBatchFile;
+	LPWSTR lpLogFile;
 
 	if(nArgs > 1) {
 		lpBatchFile = szArglist[1];
+	}
+	else {
+		return 0;
+	}
+
+	if(nArgs > 2) {
+		lpLogFile = szArglist[2];
 	}
 	else {
 		return 0;
@@ -38,6 +49,10 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 	if(_tcslen(szComSpec) == 0) {
 		return 0;
 	}
+
+	if(_wfopen_s(&fl_log, lpLogFile, L"w,ccs=UTF-8") != 0) {
+		return 0;
+	}		 
 
 	STARTUPINFO StartupInfo;
 	PROCESS_INFORMATION ProcessInfo;
@@ -49,24 +64,71 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 	saAttr.bInheritHandle = TRUE; 
 	saAttr.lpSecurityDescriptor = NULL; 
 
+	HANDLE hChildStdinRd, hChildStdinWr,  hChildStdoutRd, hChildStdoutWr, hStdout;
+
+	// current standard out
+	hStdout = GetStdHandle(STD_OUTPUT_HANDLE); 
+	 
+	if(!CreatePipe(&hChildStdoutRd, &hChildStdoutWr, &saAttr, 0))  {	
+		fclose(fl_log);
+		return 0;
+	}
+	SetHandleInformation( hChildStdoutRd, HANDLE_FLAG_INHERIT, 0);
+
+	if(!CreatePipe(&hChildStdinRd, &hChildStdinWr, &saAttr, 0)) {		
+		fclose(fl_log);
+		return 0;
+	}
+	SetHandleInformation( hChildStdinWr, HANDLE_FLAG_INHERIT, 0);
+
+
 	memset(&StartupInfo, 0, sizeof(StartupInfo));
 	
 	StartupInfo.cb = sizeof(STARTUPINFO);
-	StartupInfo.dwFlags = STARTF_USESHOWWINDOW;
+	StartupInfo.dwFlags = STARTF_USESHOWWINDOW | STARTF_USESTDHANDLES;
 	StartupInfo.wShowWindow = SW_HIDE;
+	StartupInfo.hStdError = hChildStdoutWr;
+	StartupInfo.hStdOutput = hChildStdoutWr;
+	StartupInfo.hStdInput = hChildStdinRd;
+
 
 	TCHAR szCmd[MAX_PATH];
 
 	wsprintf(szCmd, _T("%s /c %s"), szComSpec, lpBatchFile);
-	CreateProcess( NULL, szCmd, NULL, NULL, TRUE,
+	if(CreateProcess( NULL, szCmd, NULL, NULL, TRUE,
 					CREATE_NEW_CONSOLE, 
 					NULL, 
 					NULL,
 					&StartupInfo,
-					&ProcessInfo);
+					&ProcessInfo)) {
 
-	WaitForSingleObject(ProcessInfo.hProcess,INFINITE); 
+		if (CloseHandle(hChildStdoutWr)) {
+		
+			ReadFromPipe(hChildStdoutRd);
+		}
+
+		WaitForSingleObject(ProcessInfo.hProcess, INFINITE);
+		CloseHandle(ProcessInfo.hThread);
+		CloseHandle(ProcessInfo.hProcess);
+	}
+	fclose(fl_log);
 
 	return 0;
 }
 
+void ReadFromPipe(HANDLE hRead) 
+{ 	
+   DWORD dwRead; 
+   CHAR chBuf[1024];    
+   TCHAR wszBuf[1024];
+   for (;;) 
+   { 
+	  memset(chBuf, 0, 1024);
+      if( !ReadFile( hRead, chBuf, 1024, &dwRead, NULL) || dwRead == 0) break;       
+	  
+	  ::MultiByteToWideChar( CP_ACP, 0, chBuf, strlen(chBuf)+1, 
+								wszBuf,  sizeof(wszBuf)/sizeof(wszBuf[0]) );
+
+	  _ftprintf_s(fl_log, L"%s\n", wszBuf);	  
+   } 
+} 
--- runhidden/stdafx.h
+++ runhidden/stdafx.h
@@ -12,6 +12,7 @@
 #include <windows.h>
 
 // C RunTime Header Files
+#include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
 #include <memory.h>
